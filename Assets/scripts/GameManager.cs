0931543d215fdf7bd8e7d919c14f8d8d7a3a929b
Oleg_Valerievich <vitiukoleh@gmail.com>
2017-12-20 20:28:51 +0200

    unity UI

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public Text scoreText;
+
+    enum PageState {
+            None,
+            Start,
+            GameOver,
+            Countdown
+    }
+
+    int csore = 0;
+    bool gameOver = false;
+
+    public bool GameOver { get { return gameOver; } }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+}
